05c0364e9024f045271c50fd39bdcf3242371678
Victor Hom <victor.hom16@gmail.com>
2021-05-10 08:55:04 -0400

    fix file

--- scripts/comment.js
+++ scripts/comment.js
@@ -1,3 +1,8 @@
+
+const { Octokit } = require('@octokit/action')
+const { context } = require('@actions/github')
+const core = require('@actions/core')
+
 console.log(process.env.COMMENT_ISSUE_NUMBER)
 console.log(process.env.PR_BODY, "comment body")
 console.log(process.env.ISSUE_URL, "issue url")
