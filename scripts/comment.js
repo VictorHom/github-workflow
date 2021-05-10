df6ee766c9ecf5a9a1e3bb8214fe0d93c5add035
Victor Hom <victor.hom16@gmail.com>
2021-05-10 08:49:49 -0400

    print data from pull request

--- scripts/comment.js
+++ scripts/comment.js
@@ -1,3 +1,13 @@
 console.log(process.env.COMMENT_ISSUE_NUMBER)
 console.log(process.env.PR_BODY, "comment body")
 console.log(process.env.ISSUE_URL, "issue url")
+console.log(process.env.PR_URL, "pr url")
+
+getPRInfo()
+
+async function getPRInfo() {
+  const octokit = new Octokit()
+
+  const { data } = await octokit.request(process.env.PR_URL);
+  console.log(data)
+}
