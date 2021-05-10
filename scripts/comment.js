72a1b6ce27e8ccb662caf6311fc4480c5fca575e
Victor Hom <victor.hom16@gmail.com>
2021-05-10 08:59:52 -0400

    print pr data

--- scripts/comment.js
+++ scripts/comment.js
@@ -6,13 +6,12 @@ const core = require('@actions/core')
 console.log(process.env.COMMENT_ISSUE_NUMBER)
 console.log(process.env.PR_BODY, "comment body")
 console.log(process.env.ISSUE_URL, "issue url")
-console.log(process.env.PR_URL, "pr url")
 
 getPRInfo()
 
 async function getPRInfo() {
   const octokit = new Octokit()
 
-  const { data } = await octokit.request(process.env.PR_URL);
+  const { data } = await octokit.request(process.env.ISSUE_URL.replace("issues", "pulls");
   console.log(data)
 }
